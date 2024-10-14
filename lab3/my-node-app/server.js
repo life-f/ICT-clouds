ce27e8a368082127b072bbf9538741239481adec
Margarita Vasileva <vasileva@dfsystems.ru>
2024-10-14 18:50:03 +0300

    Update server.js

--- lab3/my-node-app/server.js
+++ lab3/my-node-app/server.js
@@ -1,20 +1,11 @@
-// server.js
 const http = require('http');
 
-// Создаем сервер
 const server = http.createServer((req, res) => {
-  // Устанавливаем заголовки ответа
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
-  // Отправляем ответ
   res.end('Hello, World!\n');
 });
-
-// Указываем порт, на котором сервер будет работать
 const PORT = 3000;
-
-// Запускаем сервер
 server.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
